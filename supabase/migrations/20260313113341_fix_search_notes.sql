ad8804b89f99a0d5b31f69cb9ecbe67e7f277e4b
ugouhar <ushran987@gmail.com>
2026-03-13 17:10:02 +0530

    fix search notes query

---
+++ supabase/migrations/20260313113341_fix_search_notes.sql
@@ -0,0 +1,6 @@
+create or replace function search_notes(search_query text) returns setof "ai-knowledge-base-table" language sql stable as $$
+select *
+from "ai-knowledge-base-table"
+where title ilike '%' || search_query || '%'
+    or body ilike '%' || search_query || '%';
+$$;
