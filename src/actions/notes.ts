4c67417dfe7356116f18dbad1b5e6233e5d95351
ugouhar <ushran987@gmail.com>
2026-03-26 13:21:08 +0530

    Invalidate cache

--- src/actions/notes.ts
+++ src/actions/notes.ts
@@ -4,16 +4,19 @@
 import { generateEmbedding } from "@/lib/ai/embeddings";
 import { createNote, deleteNote, updateNote } from "@/lib/db/notes.repository";
 import { Note } from "@/types/notes";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function createNoteAction(
   note: Pick<Note, "title" | "body">,
 ): Promise<void> {
   const embedding = await generateEmbedding(note.title + " " + note.body);
   await createNote({ ...note, embedding });
+  revalidateTag("notes", "max");
 }
 
 export async function deleteNoteAction(id: number): Promise<void> {
   await deleteNote(id);
+  revalidateTag("notes", "max");
 }
 
 export async function updateNoteAction(
@@ -24,4 +27,6 @@ export async function updateNoteAction(
     updatedNote.title + " " + updatedNote.body,
   );
   await updateNote(id, { ...updatedNote, embedding });
+  revalidateTag("notes", "max");
+  revalidatePath("/notes/[noteId]", "page");
 }
